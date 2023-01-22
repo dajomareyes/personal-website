f1bb2509cd6ff113c856f2e66c5d0aff402eded3
David Reyes <davidjoshuareyes@gmail.com>
2023-01-21 21:26:28 -0500

    revamp

--- src/components/Section/section.tsx
+++ src/components/Section/section.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import PropTypes from 'prop-types';
-import { Paper, Typography } from "@mui/material";
+import { Avatar, Chip, Grid, Stack, Typography } from "@mui/material";
 import { styled } from "@mui/system";
+import Image from "../image";
 
 const StyledDiv = styled('div')((props) => ({
     display: 'grid',
@@ -9,7 +9,7 @@ const StyledDiv = styled('div')((props) => ({
     overflowY: 'hidden',
     color: 'black',
     width: '100vw',
-    height: '100vh',
+    height: '90vh',
     margin: 0,
     padding: 0,
 }));
@@ -19,13 +19,41 @@ const Section = (props) => {
 
     return (
         <StyledDiv>
-            <Typography
-                textAlign={'left'}
-                lineHeight={'16px'}
-                justifyContent={'center'}>
-                Hi, I'm David.<br />
-                Software Engineer
-            </Typography>
+            <Grid
+                container
+                justifyContent={"center"}
+                alignItems={"center"}
+                spacing={2}>
+                <Grid item>
+                    <Image />
+                </Grid>
+                <Grid item>
+                    <Stack sx={{ mb: 1, mt: 3 }}>
+                        <Typography variant="h5">
+                            Hi there! My name is David
+                        </Typography>
+                        <Typography variant="subtitle1" lineHeight="1">
+                            Software Engineer
+                        </Typography>
+                    </Stack>
+                    <Stack direction="row" spacing={1}>
+                        <Chip
+                            label="Linkedin"
+                            color="primary"
+                            component="a"
+                            href="https://www.linkedin.com/in/david-joshua-reyes-7aa50ab3/"
+                            clickable
+                        />
+                        <Chip
+                            label="Github"
+                            component="a"
+                            href="https://github.com/dajomareyes"
+                            clickable
+                        />
+                    </Stack>
+
+                </Grid>
+            </Grid>
         </StyledDiv>
     )
 };
